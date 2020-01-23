68b7dde643ec83ab87dfafd7f0e7594ab7b593cc
Titouan Mathis <titouan@studiometa.fr>
2020-01-23 14:20:33 +0100

    Improve the handling of the mapbox-gl styles

--- docs/.vuepress/components/MapboxNavigationControlDemo.vue
+++ docs/.vuepress/components/MapboxNavigationControlDemo.vue
@@ -1,15 +1,8 @@
 <template>
-  <div>
-    <mapbox-map
-      style="margin-top: 1em; height: 400px;"
-      :access-token="MAPBOX_API_KEY"
-      map-style="mapbox://styles/mapbox/streets-v11">
-      <mapbox-navigation-control position="bottom-right" />
-    </mapbox-map>
-  </div>
+  <mapbox-map
+    style="height: 400px;"
+    :access-token="MAPBOX_API_KEY"
+    map-style="mapbox://styles/mapbox/streets-v11">
+    <mapbox-navigation-control position="bottom-right" />
+  </mapbox-map>
 </template>
-
-<style lang="scss">
-  // Import mapbox stylesheet to get + - and compass buttons styles
-  @import '~mapbox-gl/dist/mapbox-gl';
-</style>
