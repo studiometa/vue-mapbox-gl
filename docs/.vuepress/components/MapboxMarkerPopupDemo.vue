432142da0ba06cb6cbe1804a06f06e4697cb3b6f
Titouan Mathis <titouan@studiometa.fr>
2020-01-23 12:05:05 +0100

    Add the new props and examples to the doc

---
+++ docs/.vuepress/components/MapboxMarkerPopupDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <mapbox-map
+    style="height: 400px"
+    :access-token="MAPBOX_API_KEY"
+    map-style="mapbox://styles/mapbox/streets-v11"
+    :center="[0, 0]"
+    :zoom="1">
+    <mapbox-marker :lng-lat="[0, 0]">
+      <template v-slot:popup>
+        <p>Hello world!</p>
+      </template>
+    </mapbox-marker>
+  </mapbox-map>
+</template>
