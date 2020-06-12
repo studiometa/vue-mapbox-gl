25682605005b982bc0393f8ec6e293614d2b6875
Ludovic <ludovic@studiometa.fr>
2020-06-12 10:43:18 +0200

    Add the events to MapboxGeocoder component

--- src/components/MapboxGeocoder.vue
+++ src/components/MapboxGeocoder.vue
@@ -6,6 +6,7 @@
   import mapboxgl from 'mapbox-gl';
   import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
   import { injectMap } from '../mixins/provide-inject-map';
+  import { bindEvents, unbindEvents } from '../utils/bind-events';
 
   if (!mapboxgl) {
     throw new Error('mapboxgl is not installed.');
@@ -14,6 +15,13 @@
   if (!MapboxGeocoder) {
     throw new Error('MapboxGeocoder is not installed.');
   }
+
+  /**
+   * All Map events which will be mapped/bounded to the component
+   * @see  https://docs.mapbox.com/mapbox-gl-js/api/#marker.event:dragstart
+   * @type {Array}
+   */
+  const events = [ 'loading', 'results', 'result', 'error' ];
 
   export default {
     name: 'MapboxGeocoder',
@@ -30,9 +38,12 @@
         accessToken: mapboxgl.accessToken,
         mapboxgl,
       });
+
+      bindEvents(this, this.control, events);
       this.map.addControl(this.control);
     },
     destroyed() {
+      unbindEvents(this, this.control);
       this.map.removeControl(this.control);
     },
   };
