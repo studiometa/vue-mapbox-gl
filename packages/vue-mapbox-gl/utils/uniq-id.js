dcd5f7f96dae88893fcc0f7283a27c716b227353
Titouan Mathis <titouan@studiometa.fr>
2022-06-10 09:30:23 +0200

    Delete obsolete files

--- packages/vue-mapbox-gl/utils/uniq-id.js
+++
@@ -1,9 +0,0 @@
-/**
- * Get a prefixed or not uniq id
- * @param  {String} prefix The prefix to add to the generated id
- * @return {String}        A (prefixed) uniq id
- */
-export default function uniqId(prefix = '') {
-  const uniq = new Date().getTime() + Math.floor(Math.random() * 10000 + 1);
-  return prefix ? prefix + uniq.toString(16) : uniq.toString(16);
-}
